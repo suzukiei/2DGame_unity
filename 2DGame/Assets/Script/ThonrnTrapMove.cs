d96eee6b95acf480eea452402de2664c179f30b2
AMO551 <46017258+AMO551@users.noreply.github.com>
2025-02-01 17:36:02 +0900

    Trapを作成
    槍のトラップを作成した。

---
+++ 2DGame/Assets/Script/ThonrnTrapMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThonrnTrapMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject moveTrapObj;
+    [SerializeField]
+    private bool ThonrnStop;
+    [SerializeField]
+    private float trapStop;
+    private float timer;
+    [SerializeField]
+    Animator anim;
+    private void Start()
+    {
+        ThonrnStop = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trap");
+        if (!ThonrnStop)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                Debug.Log("Trapon");
+                ThonrnStop = true;
+                anim.SetTrigger("Animation");
+                StartCoroutine(StopMoveTimer());
+            }
+        }
+    }
+    IEnumerator StopMoveTimer()
+    {
+        yield return new WaitForSeconds(3f);
+        ThonrnStop = false;
+    }
+}
