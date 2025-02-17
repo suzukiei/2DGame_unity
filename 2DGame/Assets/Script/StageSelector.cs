41c6ef0386f677f14aa86ada1f1105d900e9cb6e
Ryota_Suzuki01 <rihureing@gmail.com>
2025-02-17 21:59:49 +0900

    ステセレの内容優先

--- 2DGame/Assets/Script/StageSelector.cs
+++ 2DGame/Assets/Script/StageSelector.cs
@@ -76,20 +76,17 @@ public class StageSelector : MonoBehaviour
         //Debug.Log(currentIndex);
         //Debug.Log(stageIndexs[currentIndex].StageIndex);
         //Debug.Log(stageIndexs[currentIndex].transform.position);
+        //ダイアログが出ているときはキー操作を受け付けない
 
- 
-
-    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("joystick button 1"))
+        {
+            //Indexは0から始まるため、範囲を超えないように
+            if (stageIndexs.Length - 1 > currentIndex)
             {
-
-                //Indexは0から始まるため、範囲を超えないように
-                if (stageIndexs.Length - 1 > currentIndex)
-                {
-                    currentIndex++;
-                    SetKyori(stageIndexs[currentIndex].transform.position);
-                }
+                currentIndex++;
+                SetKyori(stageIndexs[currentIndex].transform.position);
             }
+        }    
 
             if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("joystick button 2"))
             {
