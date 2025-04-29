16355cf743b7d20991e5a7574eb30806aeeb019a
AMO551 <46017258+AMO551@users.noreply.github.com>
2025-04-29 22:41:44 +0900

    マップ修正
    1-3 土管上引っかかり
    1-3 ボス手前引っかかり
    1-3 天井乗り越え
    1-3 乗れてしまうと勘違いする背景
    1-3 背景の地面同化

--- 2DGame/Assets/Script/Isobe/Map/MoveGround.cs
+++ 2DGame/Assets/Script/Isobe/Map/MoveGround.cs
@@ -17,19 +17,29 @@ public class MoveGround : MonoBehaviour
     {
         foreach (var a in groundList)
         {
-           
+            a.GetComponent<BoxCollider2D>().isTrigger = false;
             if (tag)
             {
                 if (a.transform.position.y >= postionMax )
                     continue;
                 a.GetComponent<Rigidbody2D>().MovePosition(new Vector2(a.transform.position.x, a.transform.position.y + (moveSpeed * Time.deltaTime)));
 
+                Vector3 cameraMinPos = Camera.main.ScreenToWorldPoint(Vector3.zero); //カメラの左端の位置
+                Vector3 cameraMaxPos = Camera.main.ScreenToWorldPoint(new Vector3(1920,1080,0)); //カメラの左端の位置
+                Debug.Log(cameraMaxPos);
+                if (a.transform.position.y < cameraMinPos.y||a.transform.position.y > cameraMaxPos.y)//
+                    a.GetComponent<BoxCollider2D>().isTrigger = true;
             }
             else
             {
                 if ( a.transform.position.y <= postionMin)
                     continue;
                 a.GetComponent<Rigidbody2D>().MovePosition(new Vector2(a.transform.position.x, a.transform.position.y - (moveSpeed * Time.deltaTime)));
+
+                Vector3 cameraMinPos = Camera.main.ScreenToWorldPoint(Vector3.zero); //カメラの左端の位置
+                Debug.Log(cameraMinPos);
+                if (a.transform.position.y < cameraMinPos.y)// 2
+                    a.GetComponent<BoxCollider2D>().isTrigger = true;
             }
     
         }
